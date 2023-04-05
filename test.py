09566fc0e01e4e54fdf7e30d37a34880cefb6141
joe beeton <joe@freedonia.org.uk>
2023-04-05 13:29:52 +0100

    commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import json
+import sys
+import http.client
+import os
+import base64
+
+# get runtime information for debugging
+
+def main():
+    print("cheese")
+
+if __name__ == "__main__":
+    main()
